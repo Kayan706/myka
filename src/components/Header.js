f2ee40a7ec2b802067477d9fc2408b13eb3d3eba
Kayan706 <Kayan7066@gmail.com>
2024-08-02 00:00:04 +0300

    two

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import phone from './img/phone.png';
 import logo from './img/logo.png';
 
+
 export default function Header() {
+  const[isOpen, setOpen] = useState();
+
   return (
     <header><div className='header'>
             
             <img src={logo} width={50} height={50} />
-            <nav className='navigator'>
-                <ul>ПРОДУКЦИЯ</ul>
-                <ul>ДОСТАВКА</ul>
-                <ul>О КОМПАНИИ</ul>
-                <ul>КОНТАКТЫ</ul>
+            <nav className={`header__nav ${isOpen ? "active" : ""}`}>
+                <ul className='header__nav-list'>
+                  <li className='header__nav-item'>ПРОДУКЦИЯ</li>
+                  <li className='header__nav-item'>ДОСТАВКА</li>
+                  <li className='header__nav-item'>О КОМПАНИИ</li>
+                  <li className='header__nav-item'>КОНТАКТЫ</li>
+                </ul>
                 
-            </nav>
+                </nav> 
             <img src={phone} width={40} height={40} className=''/>
-            <div className='burger'>
-              <span></span>
-            </div>
+            <span className={`header__menu-button ${isOpen ? "active" : ""}`} onClick={()=>setOpen(!isOpen)}></span>
         </div>
 
         </header>
   )
 }
 
-document.querySelector('.burger').addEventListener('click', function() {
-  this.classList.toggle('active');
-  document.querySelector('.navigator').classList.toggle('open');
-})
