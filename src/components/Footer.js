4edce3e94c41c34d7e17a432118420fb261137a8
Kayan706 <Kayan7066@gmail.com>
2024-08-17 12:00:08 +0300

    two

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { AiOutlineMail } from "react-icons/ai";
+import { LuPhone } from "react-icons/lu";
 
 export default function Footer() {
   return (
     <footer id='footer' >
         <div className='footertext'>
-
+          <span>«Все права защищены ©»<br/> «Не является публичной офертой»</span>
+          <div className='contact'>
+            <a href='tel:+79013494949'><LuPhone />+79013494949</a>
+            <a href="mailto:td.mtk@mail.ru"><AiOutlineMail />td.mtk@mail.ru</a>
+          </div>
         </div>
     </footer>
   )
